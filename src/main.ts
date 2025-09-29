72870eebfbcb2be87286ce5f941ee124465b1d26
Srivathsan-ISC <srivathsan.sasitharan@iteqsolutioncenter.com>
2025-09-29 16:23:43 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({ origin: 'https://backend-production-4551.up.railway.app/' });
   await app.listen(4000);
 }
 bootstrap();
